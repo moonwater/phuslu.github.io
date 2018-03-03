081647a0e37ade307a48ee78bdc4c275dc8eb8a2
phuslu <phuslu@hotmail.com>
2018-03-04 00:25:19 +0800

    add CNAME

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ set -ex
 AUTOINDEX_HTML=$(pwd -P)/autoindex.html
 for DIR in . $(ls -l | grep '^d' | awk '{print $NF}'); do
     pushd ${DIR}
-    ls -phl --time-style='+%d-%b-%Y %H:%M' | awk 'NR>1' | grep -v -w 'index.html' | grep -v -w 'push.sh' | sort -k1,1 -k8,8 | awk 'BEGIN{print "<html><head><title>Index of /</title></head><body><pre>"} {printf("<a href=\"%s\">%s</a>\t%s %s %s\n", $8, $8, $6, $7, $5)} END{print "</pre></body></html>"}' >index.html
+    ls -phl --time-style='+%d-%b-%Y %H:%M' | awk 'NR>1' | grep -v -w 'index.html' | grep -v -w 'push.sh' | grep -v -w 'CNAME' | sort -k1,1 -k8,8 | awk 'BEGIN{print "<html><head><title>Index of /</title></head><body><pre>"} {printf("<a href=\"%s\">%s</a>\t%s %s %s\n", $8, $8, $6, $7, $5)} END{print "</pre></body></html>"}' >index.html
     cat ${AUTOINDEX_HTML} >>index.html
     popd
 done
